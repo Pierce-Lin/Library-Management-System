0df485297c1f6ff3e0e3c826adbb950dcd05fb1c
Pierce-Lin <806766224@qq.com>
2020-05-15 17:19:32 +0800

    注册Servlet程序

--- src/com/web/RegistServlet.java
+++ src/com/web/RegistServlet.java
@@ -1,5 +1,9 @@
 package com.web;
 
+import com.pojo.User;
+import com.servive.UserService;
+import com.servive.impl.UserServiceImpl;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -8,8 +12,27 @@ import java.io.IOException;
 
 public class RegistServlet extends HttpServlet {
 
+    private UserService userService=new UserServiceImpl();
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        //获取请求的参数
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        String email = req.getParameter("email");
+        String code = req.getParameter("code");
+        //检查验证码是否正确 要求验证码为abcd
+        if ("abcd".equalsIgnoreCase(code)){
+            if (userService.existsUsername(username)){
+                System.out.println("用户名已存在");
+                req.getRequestDispatcher("/pages/regist.html").forward(req,resp);
+            }else{
+                userService.registUser(new User(null,username,password,email));
+                req.getRequestDispatcher("/pages/regist_success.html").forward(req,resp);
+            }
+        }else{
+            System.out.println("验证码错误");
+            req.getRequestDispatcher("/pages/regist.html").forward(req,resp);
+        }
     }
 }
